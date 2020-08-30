326d57ca44f0cf1134477e08ebdc199d819a276e
Tom Baum <tom@get-grin.com>
2020-08-30 15:29:29 +0300

    Updates

---
+++ precache-manifest.f655c65eb95a866ec41f704e2619b717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5475cd18c569dc670988c23989cc5b22",
+    "url": "/qa-test/index.html"
+  },
+  {
+    "revision": "73fb757922258033e88e",
+    "url": "/qa-test/static/css/main.289324c8.chunk.css"
+  },
+  {
+    "revision": "ed9ac4393c720a4f3fec",
+    "url": "/qa-test/static/js/2.43f0a8bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qa-test/static/js/2.43f0a8bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73fb757922258033e88e",
+    "url": "/qa-test/static/js/main.7a36fb5e.chunk.js"
+  },
+  {
+    "revision": "b56c94f97a90a992c83f",
+    "url": "/qa-test/static/js/runtime-main.465ebfe3.js"
+  }
+]);
